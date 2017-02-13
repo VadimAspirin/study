17d870ab8ebf4d13da827500db7ac1c2f60988d2
VadimAspirin <aspirine@protonmail.ch>
2017-02-13 18:44:35 +0500

    new

--- lb/lb1.cs
+++ lb/lb1.cs
@@ -3,24 +3,14 @@ using System;
 namespace Laba1 {
 
 	class Point {
-		private double x, y;
+		public readonly double X, Y;
 		public Point () {
-			x = 0;
-			y = 0;
+			X = 0;
+			Y = 0;
 			}
 		public Point (double x, double y) {
-			this.x = x;
-			this.y = y;
-			}
-		public double GetX {
-			get {
-				return x;
-				}
-			}
-		public double GetY {
-			get {
-				return y;
-				}
+			X = x;
+			Y = y;
 			}
 		}
 	
@@ -36,7 +26,8 @@ namespace Laba1 {
 			}
 		public double Length {
 			get {
-				return Math.Sqrt (Math.Pow (pointSecond.GetX - pointFirst.GetX, 2) + Math.Pow (pointSecond.GetY - pointFirst.GetY, 2));
+				return Math.Sqrt (Math.Pow (pointSecond.X - pointFirst.X, 2) +
+								  Math.Pow (pointSecond.Y - pointFirst.Y, 2));
 				}
 			}
 		}
@@ -68,18 +59,15 @@ namespace Laba1 {
 		public double Area {
 		    get {
 			double p = Perimeter / 2;
-			return Math.Sqrt (p * (p - edgeFirst.Length) * (p - edgeSecond.Length) * (p - edgeLast.Length));
+			return Math.Sqrt (p * (p - edgeFirst.Length) *
+							 (p - edgeSecond.Length) * (p - edgeLast.Length));
 					}
 				}
 		public bool Right {
 			get {
-				if ((float)(Math.Pow (edgeFirst.Length, 2) + Math.Pow (edgeSecond.Length, 2)) == (float)Math.Pow (edgeLast.Length, 2)) {
-					return true;
-					}
-				else if ((float)(Math.Pow (edgeFirst.Length, 2) + Math.Pow (edgeLast.Length, 2)) == (float)Math.Pow (edgeSecond.Length, 2)) {
-					return true;
-					}
-				else if ((float)(Math.Pow (edgeLast.Length, 2) + Math.Pow (edgeSecond.Length, 2)) == (float)Math.Pow (edgeFirst.Length, 2)) {
+				if ((float)(Math.Pow (edgeFirst.Length, 2) + Math.Pow (edgeSecond.Length, 2)) == (float)Math.Pow (edgeLast.Length, 2) ||
+					(float)(Math.Pow (edgeFirst.Length, 2) + Math.Pow (edgeLast.Length, 2)) == (float)Math.Pow (edgeSecond.Length, 2) ||
+					(float)(Math.Pow (edgeLast.Length, 2) + Math.Pow (edgeSecond.Length, 2)) == (float)Math.Pow (edgeFirst.Length, 2)) {
 					return true;
 					}
 				else {
@@ -89,13 +77,10 @@ namespace Laba1 {
 			}
 		public bool Isosceles {
 			get {
-				if (edgeFirst.Length == edgeSecond.Length) {
-					if (edgeFirst.Length == edgeLast.Length) {
-						return true;
-						}
-					else {
-						return false;
-						}
+				if (edgeFirst.Length == edgeSecond.Length || 
+					edgeFirst.Length == edgeLast.Length ||
+					edgeSecond.Length == edgeLast.Length) {
+					return true;
 					}
 				else {
 					return false;
@@ -106,22 +91,15 @@ namespace Laba1 {
 
 	class MainLaba {
 		static void Main () {
-			
+		
 			Triangle[] arrayTriangle = new Triangle[7];
-			arrayTriangle[0] = new Triangle (new Point (0, 0), new Point (5.1, 0), new Point (0, 5.4)); //r
-			arrayTriangle[1] = new Triangle (new Point (0, 0), new Point (6, 0), new Point (0, 6)); //r
-			arrayTriangle[2] = new Triangle (new Point (0, 0), new Point (4, 0), new Point (0, 8)); //r
-			arrayTriangle[3] = new Triangle (new Point (0, 0), new Point (5, 0), new Point (0, 5)); //r
+			arrayTriangle[0] = new Triangle (new Point (0, 0), new Point (5.1, 0), new Point (0, 5.4));
+			arrayTriangle[1] = new Triangle (new Point (0, 0), new Point (6, 0), new Point (0, 6));
+			arrayTriangle[2] = new Triangle (new Point (0, 0), new Point (4, 0), new Point (0, 8));
+			arrayTriangle[3] = new Triangle (new Point (0, 0), new Point (5, 0), new Point (0, 5));
 			arrayTriangle[4] = new Triangle (new Point (6, 2), new Point (7, 5), new Point (0, 25));
-			double x1 = 0, y1 = 1, x2 = 4, y2 = 0,
-			x3=(x1+x2-(y1-y2)*Math.Sqrt(3))/2,
-			y3=(y1+y2-(x2-x1)*Math.Sqrt(3))/2;
-			arrayTriangle[5] = new Triangle (new Point (x1, y1), new Point (x2, y2), new Point (x3, y3)); //i
-			x1 = 0; y1 = 2; x2 = 4; y2 = 1;
-			x3=(x1+x2-(y1-y2)*Math.Sqrt(3))/2;
-			y3=(y1+y2-(x2-x1)*Math.Sqrt(3))/2;
-			arrayTriangle[6] = new Triangle (new Point (x1, y1), new Point (x2, y2), new Point (x3, y3)); //i
-			
+			arrayTriangle[5] = new Triangle (new Point (0, 0), new Point (4, 0), new Point (0, 4));
+			arrayTriangle[6] = new Triangle (new Point (0, 0), new Point (10.6, 0), new Point (0, 10.6));
 			int iRight = 0, iIsosceles = 0;
 			double sumPerimeter = 0, sumArea = 0;
 			
@@ -138,12 +116,6 @@ namespace Laba1 {
 			
 			Console.WriteLine("Средний периметр всех прямоугольных треугольников {0}", sumPerimeter / iRight);
 			Console.WriteLine("Средняя площадь всех равнобедренных треугольников {0}", sumArea / iIsosceles);
-			
-			//Triangle triangle = new Triangle (new Point (0, 0), new Point (5.1, 0), new Point (0, 5.4));
-			//Console.WriteLine(triangle.Perimeter);
-			//Console.WriteLine(triangle.Area);
-			//Console.WriteLine(triangle.Right);
-			//Console.WriteLine(triangle.Isosceles);
 			
 			Console.WriteLine("Press any key to exit.");
 			Console.ReadKey();
