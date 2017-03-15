b350917462198a7b5f36df383ba76a90817e074b
VadimAspirin <aspirine@protonmail.ch>
2017-03-15 14:21:49 +0500

    Update Game3.cs

--- lb/lb2/Game3.cs
+++ lb/lb2/Game3.cs
@@ -1,34 +1,40 @@
 using System;
 using System.Collections.Generic;
 
-namespace Laba2 {
+namespace Laba2 
+{
 
-	class Game3 : Game2 {
+	class Game3 : Game2 
+	{
 		private List<int> history;
 		private List<int> backHistory;
-		public Game3 (params int[] numbers) : base (numbers) {
+		public Game3 (params int[] numbers) : base (numbers) 
+		{
 			history = new List<int>();
 			backHistory = new List<int>();
-			}
-		public override void Shift (int value) {
+		}
+		public override void Shift (int value) 
+		{
 			base.Shift (value);
 			history.Add (value);
 			backHistory.Clear ();
-			}
-		public void Undo () {
+		}
+		public void Undo () 
+		{
 			if (history.Count == 1)
 				throw new Exception ("error: История пуста");
 			backHistory.Add (history[history.Count - 1]);
 			base.Shift (history[history.Count - 1]);
 			history.RemoveAt (history[history.Count - 1]);
-			}
-		public void Redo () {
+		}
+		public void Redo () 
+		{
 			if (backHistory.Count == 0)
 				throw new Exception ("error: История пуста");
 			Shift (backHistory[backHistory.Count - 1]);
 			backHistory.RemoveAt (backHistory[backHistory.Count - 1]);
-			}
-
 		}
 
 	}
+
+}
