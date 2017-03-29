80909bfc0a7d1659c973a26d1f7731c1344ed667
VadimAspirin <aspirine@protonmail.ch>
2017-03-29 15:00:01 +0500

    Update IPlayable.cs

--- lb/lb2/IPlayable.cs
+++ lb/lb2/IPlayable.cs
@@ -4,15 +4,17 @@ using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Laba2 {
+namespace Laba2 
+{
 
-	interface IPlayable {
+	interface IPlayable 
+	{
 		void RandomizeGame ();
 		bool CheckVictoryGame ();
 		void Shift (int value);
 		
 		int GetLength (int dimension);
 		int this [int x, int y] { get; }
-		}
-	
 	}
+	
+}
