91f8d11b98dbbb0b279d4813287d6f992f537a33
VadimAspirin <aspirine@protonmail.ch>
2017-04-24 22:47:12 +0500

    mew2

--- lb/lb7/Program.cs
+++ lb/lb7/Program.cs
@@ -10,7 +10,19 @@ namespace lb7
     {
         static void Main(string[] args)
         {
+            MaxBinaryHeap<double> maxBinaryHeap = new MaxBinaryHeap<double>();
+            maxBinaryHeap.Insert(6);
+            maxBinaryHeap.Insert(8);
+            maxBinaryHeap.Insert(12);
+            maxBinaryHeap.Insert(14);
+            maxBinaryHeap.Insert(7);
+            maxBinaryHeap.Insert(11);
+            maxBinaryHeap.Insert(9);
+            maxBinaryHeap.Insert(9);
+            maxBinaryHeap.Insert(2);
 
+            for (int i = 0; i < maxBinaryHeap.HeapSize; i++)
+                Console.WriteLine(maxBinaryHeap.ValuesOfBinaryHeap[i]);
 
         }
 
